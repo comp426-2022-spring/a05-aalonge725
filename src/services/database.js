fd692f737e4bb956a65d7883f38d91713a64e390
Abraham Alonge <aalonge@unc.edu>
2022-04-30 09:05:47 -0400

    Update database.js

--- src/services/database.js
+++ src/services/database.js
@@ -1,38 +1,43 @@
-const database = require('better-sqlite3')
+// This ensures that things do not fail silently but will throw errors instead.
+"use strict";
+// Require better-sqlite.
+const Database = require('better-sqlite3');
 
-const fs = require('fs');
-const datadir = './data/';
+// Connect to a database or create one if it doesn't exist yet.
+const db = new Database('./data/db/log.db');
 
-if (!fs.existsSync(datadir)){
-    fs.mkdirSync(datadir);
-}
-
-const logdb = new database(datadir+'log.db')
-
-const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+// Is the database initialized or do we need to initialize it?
+const stmt = db.prepare(`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
+);
+// Define row using `get()` from better-sqlite3
 let row = stmt.get();
+// Check if there is a table. If row is undefined then no table exists.
 if (row === undefined) {
-    console.log('Log database appears to be empty. Creating log database...')
-
+    // Echo information about what you are doing to the console.
+    console.log('Your database appears to be empty. I will initialize it now.');
+    // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE accesslog ( 
+        CREATE TABLE accesslog (
             id INTEGER PRIMARY KEY, 
             remoteaddr TEXT,
             remoteuser TEXT,
-            time TEXT,
+            time TEXT, 
             method TEXT,
             url TEXT,
-            protocol TEXT,
+            protocol TEXT, 
             httpversion TEXT,
             status TEXT, 
-            referrer TEXT,
-            useragent TEXT
-        );
-    `
-
-    logdb.exec(sqlInit)
+            referer TEXT,
+            useragent TEXT);
+    `;
+    // Execute SQL commands that we just wrote above.
+    db.exec(sqlInit);
+    // Echo information about what we just did to the console.
+    console.log('Your database has been initialized with a new table.');
 } else {
-    console.log('Log database exists.')
+    // Since the database already exists, echo that to the console.
+    console.log('Database exists.')
 }
-
-module.exports = logdb
+// Export all of the above as a module so that we can use it elsewhere.
+module.exports = db
